58a19d0143920aee4740b3eaf32eda6e33df304e
trtchir <tchir@att.net>
2018-06-16 16:47:33 -0400

    Added template driven form error handling of mat-error to post-create component.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,6 @@
 import { Post } from './../posts.model';
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
@@ -11,10 +12,13 @@ export class PostCreateComponent {
   enteredContent = '';
   @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
     const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
+      title: form.value.tiitle,
+      content: form.value.content
     };
     this.postCreated.emit(post);
   }
